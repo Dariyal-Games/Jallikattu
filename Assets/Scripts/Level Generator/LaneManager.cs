45926b391f3f495a0eb6732380077b632d2e8c16
Manuel <manuel@dariyalgames.com>
2017-05-06 16:27:53 +0530

    Random lane generation completed

--- Assets/Scripts/Level Generator/LaneManager.cs
+++ Assets/Scripts/Level Generator/LaneManager.cs
@@ -2,22 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LaneManager : MonoBehaviour {
 
     public GameObject[] lanePrefabs;
 
     private Transform bullTransform;
     private float spawnZ = 0;
+    private float safeZone = 40;
     private float laneLength = 37.95f;
     private int lanesOnScreen = 3;
+    private int lastPrefabIndex = 0;
+
+    private List<GameObject> activeLanes;
 
 
    	// Use this for initialization
 	void Start () {
+        activeLanes = new List<GameObject>();
         bullTransform = GameObject.FindGameObjectWithTag("Bull").transform;
 		for(int i=0; i<lanesOnScreen; i++)
         {
-            SpawnLane();
+            if (i < 2)
+                SpawnLane(0);
+            else 
+                SpawnLane();
+            
         }
 
 	}
@@ -25,9 +35,10 @@ public class LaneManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if(bullTransform.position.z>(spawnZ-lanesOnScreen *laneLength ))
+        if(bullTransform.position.z-safeZone>(spawnZ-lanesOnScreen *laneLength ))
         {
             SpawnLane();
+            DestroyLane();
         }		
 	}
 
@@ -35,8 +46,36 @@ public class LaneManager : MonoBehaviour {
     void SpawnLane(int prefabIndex = -1)
     {
         GameObject go;
-        go = Instantiate(lanePrefabs[0]) as GameObject;
+        if (prefabIndex == -1)
+            go = Instantiate(lanePrefabs[RandomPrefabIndex()]) as GameObject;
+        else
+            go = Instantiate(lanePrefabs[prefabIndex]) as GameObject;
+        go.transform.SetParent(transform);
         go.transform.position = Vector3.forward * spawnZ;
         spawnZ += laneLength;
+        activeLanes.Add(go);
+    }
+
+
+    void DestroyLane()
+    {
+        Destroy(activeLanes [0]);
+        activeLanes.RemoveAt(0);
+    }
+
+    private int RandomPrefabIndex()
+    {
+        if (lanePrefabs.Length <= 1)
+            return 0;
+
+        int randomIndex = lastPrefabIndex;
+        while (randomIndex==lastPrefabIndex)
+        {
+            randomIndex = Random.Range(0, lanePrefabs.Length);
+        }
+
+        lastPrefabIndex = randomIndex;
+        return randomIndex;
+
     }
 }
