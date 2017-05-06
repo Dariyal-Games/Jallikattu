2b498662074a03deb1597c81569787bdbf9728b6
MUBARAK\mubarak <mubarak.ibrahim@dariyalgames.com>
2017-05-06 18:19:38 +0530

    crowdsystem(attach man to bull)

--- Assets/CrowdSystem/CrowdSystem.cs
+++ Assets/CrowdSystem/CrowdSystem.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class CrowdSystem : MonoBehaviour {
 
-	void OnTriggerEnter(Collider other)
+	public GameObject Bull;
+	public Transform personToAttach;
+
+
+
+	void Awake()
 	{
-		if(other.gameObject.tag=="Player"){
-			
+		
+		//bullposi = GetComponent<BullPosition> ();
+
+	}
+
+   
+    public void Start()
+    {
+		//PosPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+    public void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.tag == "Player")
+        {
+			//transform.rotation=Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(PosPlayer.position-transform.position,CrowdRotateSpeed*Time.deltaTime));
+			 //transform.position += transform.position * CrowdMoveSpeed*Time.deltaTime;
+			//OnTriggerEnter()
+
+			//transform.position = Bull.transform.position;
+			var attachPoint = other.gameObject.GetComponent<BullController>().attachPoint;
+			var pattachPoint = personToAttach.GetCo
+	    }
+	}
+		
+
+	void AttachToPoint()
+	{
+		
 	}
 
 }
