b171355615cfc81d4b461d3867036a850ef3f369
Lloyd Benjamin <lloydprakashbenjamin@gmail.com>
2017-05-06 16:46:59 +0530

    Proper UI Sign In and Sign Out

---
+++ Assets/Scripts/Menu System/Authenticator.cs
@@ -0,0 +1,33 @@
+﻿using GooglePlayGames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Authenticator : MonoBehaviour
+{
+
+
+
+    private void Start()
+    {
+        PlayGamesPlatform.Activate();
+    }
+    public void SignIn()
+    {
+        if (!Social.localUser.authenticated)
+        {
+            Social.localUser.Authenticate((bool success) =>
+            {
+                if (success)
+                {
+                    Debug.Log("User Signed In");
+                }
+                else
+                {
+                    Debug.LogError("Error signing in");
+                }
+            });
+        }
+    }
+}
--- Assets/Scripts/Menu System/MainMenu.cs
+++ Assets/Scripts/Menu System/MainMenu.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("GameScene");
+        // for now we havnt any option list so when  it is ready we will replace with the option scene name.
+    }
+
 }
