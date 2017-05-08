4f7c75717a63e3b00dc7c2ed1ef6093790a16d1e
Lloyd Benjamin <lloydprakashbenjamin@gmail.com>
2017-05-08 15:59:00 +0530

    Added Score Manger

--- Assets/Scripts/Menu System/ScoreManager.cs
+++ Assets/Scripts/Menu System/ScoreManager.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour {
+public class ScoreManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static ScoreManager _instance;
+    private int playerScore;
+
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<ScoreManager>();
+
+                if (_instance == null)
+                {
+                    _instance = new GameObject("Score Manager").AddComponent<ScoreManager>();
+                    //DontDestroyOnLoad(_instance.gameObject);
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+
+
+    public void AdjustScore(int num)
+    {
+        playerScore += num;
+    }
+
+    public int GetScore()
+    {
+        return playerScore;
+    }
 }
