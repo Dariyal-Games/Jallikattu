e7f56d8e44b04fee2ce8a3c65093ef04645d2d52
raidenlolt <raidenlolt@gmail.com>
2017-05-16 15:11:43 +0530

    Game balance changes and bug fixes

--- Assets/Scripts/Stats/UnityConversionFactors.cs
+++ Assets/Scripts/Stats/UnityConversionFactors.cs
@@ -6,9 +6,9 @@
 	{
 		public static readonly float Acceleration = 10f;
 		public static readonly float Deceleration = 10f;
-		public static readonly float TurnRate = 10f;
-		public static readonly float Speed = .5f;
-		public static readonly float Shrug = 10f;
+		public static readonly float TurnRate = 5f;
+		public static readonly float Speed = .2f;
+		public static readonly float Shrug = 5f;
 	}
 
 
